1d1c377d4bb7d260ecaceef386b9ba1eb0cebf40
Sinser Thomas <dbsjtom@gmail.com>
2022-03-04 19:22:38 +0300

    ну класс добавила

---
+++ DaGame/DaBosss.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaGame
+{
+    internal class DaBosss
+    {
+        public static string BossName;
+        public static int BossMaxHP = 50;
+        public static int BossHP = 50;
+        public static int BossAttack = 5;
+        public static int BossExp;
+    }
+}
